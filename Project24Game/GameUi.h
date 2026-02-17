1639e84d012f61df42b03ccdb5041b9150ce86f0
SolidGA5 <154576174+SolidGA5@users.noreply.github.com>
2026-02-17 15:31:43 +0700

    Add files via upload
    Create windows with Reactable buttons , mainly I have done was header file to make file more easy development

---
+++ Project24Game/GameUi.h
@@ -0,0 +1,54 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+using namespace std;
+
+float TittleGameSize = 100;
+string Fontloc = "C:/Windows/Fonts/arial.ttf";
+float windowSize_x = 1600;
+float WindowSize_y = 900;
+sf::Font font(Fontloc); //import font_style variable (pull form location)  
+
+class buttonBuild{
+    public:
+        float posBox_x;
+        float posBox_y;
+        float FontSize;
+        float buttonSize_x;
+        float buttonSize_y;
+        float shiftX;
+        float shiftY;
+        string name;
+        sf::RectangleShape builtButton(); 
+        sf::Text txtBox();
+};
+
+class Title{
+    string Title_string = "Game24";
+    public:
+        sf::Text TitleName();
+};
+
+
+sf::Text Title::TitleName(){
+    sf::Text Title_24(font,Title_string,TittleGameSize);
+    Title_24.setPosition({windowSize_x/2-TittleGameSize-50,50});
+    return Title_24;
+};
+
+
+
+sf::RectangleShape buttonBuild::builtButton(){
+    sf::RectangleShape button({buttonSize_x,buttonSize_y}); //Create button variable with size
+    button.setPosition({posBox_x,posBox_y}); 
+    button.setFillColor(sf::Color::Blue);
+    return button;
+};
+
+sf:: Text buttonBuild::txtBox(){
+    sf::Text Text(font,name,FontSize);
+    Text.setPosition({posBox_x+(buttonSize_x/2)+shiftX,posBox_y+(buttonSize_y/2)+shiftY});
+    return Text;
+}
+
+
---
+++ Project24Game/UI24.cpp
@@ -0,0 +1,70 @@
+#include "GameUi.h"
+
+
+int main()
+{   
+    // Build Windows Frame
+    sf::RenderWindow window(sf::VideoMode({1600,900}), "GAME24"); //Create window variable with size and tittle name'
+    Title tilte;
+
+
+    // button24
+    buttonBuild button24 = {
+        .posBox_x = windowSize_x/6,
+        .posBox_y = WindowSize_y/(float)2, // 2.5 when no float was assian as double type
+        .FontSize = 75,
+        .buttonSize_x = 225,
+        .buttonSize_y = 75,
+        .shiftX = -110,
+        .shiftY = -50,
+        .name = "Game24"
+    };
+
+    // buttonN
+    buttonBuild buttonN = {
+        .posBox_x = windowSize_x*4/6,
+        .posBox_y = WindowSize_y/(float)2, // 2.5 when no float was assian as double type
+        .FontSize = 75,
+        .buttonSize_x = 225,
+        .buttonSize_y = 75,
+        .shiftX = -110,
+        .shiftY = -50,
+        .name = "Random"
+    };
+
+    sf:: RectangleShape button_24 = button24.builtButton();
+    sf:: RectangleShape button_N = buttonN.builtButton();
+    
+    while (window.isOpen()) // using to make window always open 
+    {
+        while (const std::optional event = window.pollEvent()) // Closing windows functions
+        {
+            if (event->is<sf::Event::Closed>()) window.close(); // "close requested" event: we close the window 
+        }// 
+        auto mouse_pos = sf::Vector2f(sf::Mouse::getPosition(window));
+        if (button_24.getGlobalBounds().contains(mouse_pos)) // check button is coline with mouse : augmentNeedtoCheck.getGlobalBound().contains(whatIscheckwith); >> getGlobalBound() = check coline
+        {
+            button_24.setFillColor(sf::Color(0,75,22)); // we can create animated or click to process
+        }else{
+            button_24.setFillColor(sf::Color::Blue); // reset went not set
+        }
+        if (button_N.getGlobalBounds().contains(mouse_pos)) // check button is coline with mouse : augmentNeedtoCheck.getGlobalBound().contains(whatIscheckwith); >> getGlobalBound() = check coline
+        {
+            button_N.setFillColor(sf::Color(0,75,22)); // we can create animated or click to process
+        }else{
+            button_N.setFillColor(sf::Color::Red); // reset went not set
+        }
+
+        
+        // windown.draw is order by line to line upper = under
+        window.clear(sf::Color::Black);
+        window.draw(tilte.TitleName());
+        window.draw(button_24);
+        window.draw(button_N);
+        window.draw(button24.txtBox());
+        window.draw(buttonN.txtBox());    
+        
+        // end the current frame
+        window.display();
+    }
+}
