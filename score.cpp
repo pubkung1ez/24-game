d013354ce017aa956e3524d5fb03e0a58055b943
pubkung1ez <tirapan_30506@sysp.ac.th>
2026-02-17 16:40:59 +0700

    suss

--- score.cpp
+++ score.cpp
@@ -2,37 +2,71 @@
 #include <thread>
 #include <chrono>
 #include <atomic>
-
+#include <string>
 using namespace std;
 
-atomic<int> timeLeft(60);
+atomic<int> timeLeft(10);
 atomic<bool> running(true);
+atomic<int> score(0);
+atomic<int> streak(1);
+atomic<bool> answeredCorrect(true);
+atomic<int> difficulty(1);
+atomic<bool> modetimer(true);
+atomic<int> scoremax(0);
 
-void timer() {
+void timer_mode_normal() {
+    while (running && timeLeft > 0) {
 
-    while (running) {
+        cout << "\nTime Left: " << timeLeft << " sec" << flush;
+        this_thread::sleep_for(chrono::seconds(1));
 
-        if (timeLeft > 0) {
-            cout << "\rTime Left: " << timeLeft-- << " sec" << flush;
-            this_thread::sleep_for(chrono::seconds(1));
+        timeLeft--;
+    }
+
+    if (timeLeft == 0) {
+        cout << "\nTime's up!\n";
+        running = false;
+        streak = 0;
+    }
+}
+
+void point_mode_normal(){
+    while(answeredCorrect){
+        score += 10*(difficulty/10)*(streak/10);
+        if(score > scoremax){
+            scoremax = score;
         }
-        else {
-            cout << "\nTime's up!\n";
-            running = false;
-        }
+    }
+    if(!answeredCorrect){
+        answeredCorrect = true;
+        score -= 10*(difficulty/15)*(streak/11);
     }
 }
 
 int main() {
-
-    thread t(timer);
-
+    if(modetimer){
+    thread t1(timer_mode_normal);
+    thread t2(point_mode_normal);
+    if("ans == incorrect"){
+        answeredCorrect = false;
+    }
     //
         //code by arm
 
 
     //
+    cout << "\nsss\n";
+    t1.join();
+    t2.join();
+    }else{
+    thread t2(point_mode_normal);
+    if("ans == incorrect"){
+        answeredCorrect = false;
+    }
 
-    t.join();
-    return 0;
+
+    t2.join();
+    }
+
+return 0;
 }
